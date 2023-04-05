03c554696809856298aa8bbe4baebaa6aaf8b9af
Seremba Patrick <pserembae@gmail.com>
2023-04-05 12:18:55 +0300

    create UserRepository interface

---
+++ web/src/main/java/com/rungroup/web/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.rungroup.web.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rungroup.web.models.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+	
+	UserEntity findByEmail(String email);
+	
+	UserEntity findByUsername(String username);
+}
