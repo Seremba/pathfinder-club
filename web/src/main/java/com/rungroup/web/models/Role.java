843d07ed33114b184e1b413c9b6f0a18ee9dc5da
Seremba Patrick <pserembae@gmail.com>
2023-04-05 12:17:07 +0300

    create Role model

---
+++ web/src/main/java/com/rungroup/web/models/Role.java
@@ -0,0 +1,31 @@
+package com.rungroup.web.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	@ManyToMany(mappedBy = "roles")
+	private List<UserEntity> users = new ArrayList<>();
+}
